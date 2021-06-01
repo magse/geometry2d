62ba8bc3318f4c422e7168f3c6327b2aa7dc2433
Magnus Sethson <magnus@sethson.se>
2021-06-01 11:27:29 +0200

    Required packages changed
    Catch2 dependency removed due to unclear install process

--- test/test_geometry2d.cpp
+++ test/test_geometry2d.cpp
@@ -3,24 +3,20 @@
 #include "geometry2d.hpp"
 #include <iostream>
 
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
 
-TEST_CASE("test_float")
-{
-  int ret=geometry2d::geometry2d_test<float>(std::cout);
-  REQUIRE(ret == 0);
-}
+int main(int argc, char** argv) {
+	
+	int ret=0;
+	
+	ret=geometry2d::geometry2d_test<float>(std::cout);
+	assert(0==ret);
+	
+	ret=geometry2d::geometry2d_test<double>(std::cout);
+	assert(0==ret);
 
-TEST_CASE("test_double")
-{
-  int ret=geometry2d::geometry2d_test<double>(std::cout);
-  REQUIRE(ret == 0);
-}
+	ret=geometry2d::geometry2d_test<long double>(std::cout);
+	assert(0==ret);
 
-TEST_CASE("test_long_double")
-{
-  int ret=geometry2d::geometry2d_test<long double>(std::cout);
-  REQUIRE(ret == 0);
+	return 0;
 }
 
